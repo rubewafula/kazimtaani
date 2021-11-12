27acf413079a7361eed592bad0cfb62a1cf26a1b
Levidic <levkisaka@gmail.com>
2021-11-12 17:11:12 +0300

    Rename logo name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
         <nav  class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img style="height:50px;" src="{{ asset('img/logo.JPEG') }}"/>
+                    <img style="height:50px;" src="{{ asset('img/logo.jpeg') }}"/>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
