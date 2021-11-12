6b8251f98b16e3b601204a42c65d4fad77a30baf
Levidic <levkisaka@gmail.com>
2021-11-12 14:03:47 +0300

    Fix the fuck up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Snippet - BBBootstrap</title>
+    <title>Kazi Mtaani Registration</title>
     <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet'>
     <link href='' rel='stylesheet'>
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
@@ -30,6 +30,9 @@
 
       .mt-200 {
         margin-top: 200px
+      }
+      .btn-toolbar{
+        padding: 8px;
       }
     </style>
   </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,42 @@
 @extends('layouts.app')
  @section('content')
-    <div style="margin: top 200px;" class="container">
-      <div id="smartwizard">
+    <div style="margin-top:50px;" class="container">
+      <div  style="padding:10px;" id="smartwizard">
         <ul>
-          <li><a href="#step-1">Step 1<br /><small>Account Info</small></a></li>
-          <li><a href="#step-2">Step 2<br /><small>Personal Info</small></a></li>
-          <li><a href="#step-3">Step 3<br /><small>Payment Info</small></a></li>
-          <li><a href="#step-4">Step 4<br /><small>Confirm details</small></a></li>
+          <li><a href="#step-1">Step 1<br /><small>Personal Details</small></a></li>
+          <li><a href="#step-2">Step 2<br /><small>National ID Details</small></a></li>
+          <li><a href="#step-3">Step 3<br /><small>Location Details</small></a></li>
+          <li><a href="#step-4">Step 4<br /><small>Education</small></a></li>
+          <li><a href="#step-5">Step 5<br /><small>Skill level</small></a></li>
+          <li><a href="#step-5">Step 6<br /><small>Alternative payment details</small></a></li>
+          
         </ul>
         <div>
-          <div id="step-1">
+          <div style="padding: 15px;" id="step-1">
             <div class="row">
-              <div class="col-md-6"><input type="text" class="form-control" placeholder="Name" required></div>
-              <div class="col-md-6"><input type="text" class="form-control" placeholder="Email" required></div>
+              <div class="col-md-6">
+                <label class="form-label" for="">First Name</label>
+                <input type="text" class="form-control" name="first_name" placeholder="Enter Your First Name" required>
+              </div>
+              <div class="col-md-6">
+                <label class="form-label" for="">Middle Name</label>
+                <input type="text" class="form-control" name="middle_name" placeholder="Enter Your Middle Name" required>
+              </div>
             </div>
             <div class="row mt-3">
-              <div class="col-md-6"><input type="text" class="form-control" placeholder="Password" required></div>
-              <div class="col-md-6"><input type="text" class="form-control" placeholder="Repeat password" required></div>
+              <div class="col-md-6">
+                <label class="form-label" for="">Last Name</label>
+                <input type="text" class="form-control" name="last_name" placeholder="Enter Your Last Name" required>
+              </div>
+              
+              <div class="col-md-6">
+                <label class="form-label" for="">Date Of Birth</label>
+                <input type="date" class="form-control" name="dob"  required>
+              </div>
+
             </div>
           </div>
-          <div id="step-2">
+          <div style="padding: 15px;" id="step-2">
             <div class="row">
               <div class="col-md-6"><input type="text" class="form-control" placeholder="Address" required></div>
               <div class="col-md-6"><input type="text" class="form-control" placeholder="City" required></div>
@@ -29,7 +46,7 @@
               <div class="col-md-6"><input type="text" class="form-control" placeholder="Country" required></div>
             </div>
           </div>
-          <div id="step-3" class="">
+          <div style="padding: 15px;" id="step-3" class="">
             <div class="row">
               <div class="col-md-6"><input type="text" class="form-control" placeholder="Card Number" required></div>
               <div class="col-md-6"><input type="text" class="form-control" placeholder="Card Holder Name" required></div>
@@ -39,10 +56,25 @@
               <div class="col-md-6"><input type="text" class="form-control" placeholder="Mobile Number" required></div>
             </div>
           </div>
-          <div id="step-4" class="">
+
+          <div style="padding: 15px;" id="step-4" class="">
             <div class="row">
               <div class="col-md-12"><span>Thanks For submitting your details with BBBootstrap.com. we will send you a confirmation email. We will review your details and revert back.</span></div>
             </div>
+            </div>
+
+            <div style="padding: 15px;" id="step-5" class="">
+              <div class="row">
+                <div class="col-md-12"><span>Thanks For submitting your details with BBBootstrap.com. we will send you a confirmation email. We will review your details and revert back.</span></div>
+              </div>
+            </div>
+
+            <div style="padding: 15px;" id="step-6" class="">
+              <div class="row">
+                <div class="col-md-12"><span>Thanks For submitting your details with BBBootstrap.com. we will send you a confirmation email. We will review your details and revert back.</span></div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
